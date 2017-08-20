6a589462ff11517e6aaa7826f962dc0fd62b6442
irhamputra <irhamputraprasetyo@gmail.com>
2017-08-20 21:32:55 +0200

    Backend finished

--- router.js
+++ router.js
@@ -1,3 +1,13 @@
-/**
- * Created by MacbookPro on 8/20/17.
- */
+import express, { Router } from 'express';
+// Import index action from movies controller
+import { index } from './controllers/movies';
+
+// Initialize the router
+const router = Router();
+
+// Handle /movies.json route with index action from movies controller
+router.route('/movies.json')
+    .get(index);
+
+export default router;
+
